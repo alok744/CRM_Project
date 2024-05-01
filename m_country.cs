8a931ee7b15a29a9523a635325949c8d59eaf77e
Alok Sharma <56442060+alok744@users.noreply.github.com>
2024-05-01 17:50:09 +0530

    modal

---
+++ m_country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Loginpage_project.Models
+{
+    public class m_country
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int country_id { get; set; }
+        public string country_name { get; set; }
+        public DateTime created_date { get; set; }
+        public string created_by { get; set; }
+        public bool del_status { get; set; }
+    }
+}
