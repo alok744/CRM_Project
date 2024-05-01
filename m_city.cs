dcb9894ab1e7563ca56024bcf1f80a5bb6c9558f
Alok Sharma <56442060+alok744@users.noreply.github.com>
2024-05-01 17:50:26 +0530

    Add files via upload

---
+++ m_city.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Loginpage_project.Models
+{
+    public class m_city
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int city_id { get; set; }
+        public int state_id { get; set; }
+        public string city_name { get; set; }
+        public DateTime created_date { get; set; }
+        public string created_by { get; set; }
+        public bool del_status { get; set; }
+        [ForeignKey("state_id")]
+        public m_state m_state { get; set; }
+    }
+}
