554fa9ed92786db6ed7c1f840c2a02ecffce80a3
Alok Sharma <56442060+alok744@users.noreply.github.com>
2024-05-01 17:51:09 +0530

    Add files via upload
    DBContext

---
+++ myloginpage_Dbcontext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Loginpage_project.Models
+{
+    public class myloginpage_Dbcontext : DbContext
+    {
+        public myloginpage_Dbcontext(DbContextOptions<myloginpage_Dbcontext> options): base(options)
+        {
+
+        }
+        public DbSet<Loginpage> Loginpage { get; set; }
+        public DbSet<m_country> m_country { get; set; }
+        public DbSet<m_state> m_state { get; set; }
+        public DbSet<m_city> m_city { get; set; }
+           
+    }
+}
