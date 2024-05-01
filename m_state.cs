e8df51e2279ffc8f4128dd42b30a027a5bef3c45
Alok Sharma <56442060+alok744@users.noreply.github.com>
2024-05-01 17:50:46 +0530

    Add files via upload

---
+++ m_state.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Loginpage_project.Models
+{
+    public class m_state
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int state_id { get; set; }
+        public int country_id { get; set; }
+        public string state_name { get; set; }
+        [ForeignKey("country_id")]
+        public m_country m_country { get; set; }
+        public DateTime created_date { get; set; }
+        public string created_by { get; set; }
+        public bool del_status { get; set; }
+
+    }
+}
